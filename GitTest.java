f19fad5d7b38e6dc5784bb736661b33ce879a723
Cecinestpas Unepipe <forsberg.bjorn.a@gmail.com>
2021-12-01 23:20:58 +0100

    added another method printing a Git command for terminal

--- GitTest.java
+++ GitTest.java
@@ -3,6 +3,16 @@ public class GitTest {
 	public static void main(String[] args) {
 
 		System.out.println("First Git code");
+		printGitCommands();
+
 	}
-	
+
+	public stativ void printGitCommands() {
+
+		System.out.println("git add .");
+		System.out.println("add all changes");
+	}
+
+
+
 }
